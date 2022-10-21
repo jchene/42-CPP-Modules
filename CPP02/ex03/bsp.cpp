dd3e1372fb36ef86a07a19cf3cc64adf52130bb8
Julien CHÊNE <jchene@student.42.fr>
2022-10-21 13:17:23 +0200

    finished cpp02 with ex03 final

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -6,32 +6,34 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 14:40:38 by jchene            #+#    #+#             */
-/*   Updated: 2022/10/20 12:57:02 by jchene           ###   ########.fr       */
+/*   Updated: 2022/10/21 13:16:55 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
+#define POINT_EPSILON 0.0371094f
 
-float fAbs(float nb) { return (nb < 0 ? nb * -1 : nb); }
-
-Fixed FiAbs(Fixed nb) { return (nb < 0 ? nb * -1 : nb); }
+float floatAbs(float nb) { return (nb < 0 ? nb * -1 : nb); }
 
 float getTriangleArea(Point const a, Point const b, Point const c)
 {
 	float c1 = (a.getX() * (b.getY() - c.getY())).toFloat();
 	float c2 = (b.getX() * (c.getY() - a.getY())).toFloat();
 	float c3 = (c.getX() * (a.getY() - b.getY())).toFloat();
-	return (fAbs((c1 + c2 + c3) / 2));
+	return (floatAbs((c1 + c2 + c3) / 2));
 }
 
-bool isBorder(Point const a, Point const b, Point const point)
+bool isBorder(Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed crossProduct;
-	crossProduct = (point.getY()-a.getY()) * (b.getX()-a.getX()) - (point.getX()-a.getX()) * (b.getY()-a.getY());
-	if (FiAbs(crossProduct) <= 0)
-		std::cout << "Is on border" << std::endl;
-	return (FiAbs(crossProduct) <= 0);
+	if (!(getTriangleArea(a, b, point) > POINT_EPSILON
+			&& getTriangleArea(a, c, point) > POINT_EPSILON
+			&& getTriangleArea(b, c, point) > POINT_EPSILON))
+	{
+		std::cout << "Point is in border of triangle" << std::endl;
+		return (true);
+	}
+	return (false);
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
@@ -45,11 +47,10 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 	}
 	areaSum = areaSum + getTriangleArea(a, c, point);
 	areaSum = areaSum + getTriangleArea(b, c, point);
-	std::cout << "diff: " << fAbs(triangleArea - areaSum) << std::endl;
 	if (triangleArea != areaSum)
 	{
 		std::cout << "Point not in triangle" << std::endl;
 		return (false);
 	}
-	return (!(isBorder(a, b, point) || isBorder(a, c, point) || isBorder(b, c, point)));
+	return (!(isBorder(a, b, c, point)));
 }
--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,24 +6,11 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 15:13:12 by jchene            #+#    #+#             */
-/*   Updated: 2022/10/20 12:48:46 by jchene           ###   ########.fr       */
+/*   Updated: 2022/10/21 13:16:36 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
-
-/*int main(void)
-{
-	Point a(-25.3f, 21.6f);
-	Point b(12, 13);
-	Point c(4, -3);
-	Point point(-18.2f, 4.5f);
-	Point point2(-5, 12);
-	bool inTriangle = bsp(a, b, c, point2);
-
-	std::cout << "Point is in triangle: " << inTriangle << std::endl;
-	return (0);
-}*/
 
 int main(void)
 {
@@ -33,9 +20,13 @@ int main(void)
 	Point point(-18.2f, 4.5f);
 	Point point2(-5, 12);
 	Point point3(4, -3);
-	Point point4(-4.91f, 16.9);
-	bool inTriangle = bsp(a, b, c, point3);
-
-	std::cout << "Point is in triangle: " << inTriangle << std::endl;
+	Point point4(-4.9f, 16.9f);
+	Point point5(-4.9145f, 16.8998f);
+	Point point6(-4.914508919431277f, 16.89985996533804f);
+	Point point7(-4.914478482834081f,16.899875598948917f);
+	Point point8(-4.91452f,16.89974f);
+	bool inTriangle = bsp(a, b, c, point5);
+	if (inTriangle)
+		std::cout << "Point is in triangle" << std::endl;
 	return (0);
 }
