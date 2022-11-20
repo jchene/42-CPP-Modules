e76872f0ebbb1504a147d7ba486c1d6237bb9ac8
Julien CHÊNE <jchene@student.42.fr>
2022-11-20 19:38:16 +0100

    starting cpp05

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -6,40 +6,95 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 19:37:18 by jchene            #+#    #+#             */
-/*   Updated: 2022/11/19 19:56:30 by jchene           ###   ########.fr       */
+/*   Updated: 2022/11/20 19:38:03 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+//EXCEPTIONS
+Bureaucrat::GradeTooHighException::GradeTooHighException(){
+	if (MSGS > 2)
+		std::cout << "Bureaucrat::GradeTooHighException default c.tor called" << std::endl;}
+
+Bureaucrat::GradeTooHighException::GradeTooHighException(const Bureaucrat::GradeTooHighException &ref){
+	if (MSGS > 2)
+		std::cout << "Bureaucrat::GradeTooHighException copy c.tor called" << std::endl;}
+
+Bureaucrat::GradeTooHighException &Bureaucrat::GradeTooHighException::operator=(const Bureaucrat::GradeTooHighException &rhs){
+	if (MSGS > 2)
+		std::cout << "Bureaucrat::GradeTooHighException copy assignment o.tor called" << std::endl;}
+
+Bureaucrat::GradeTooHighException::~GradeTooHighException(){
+	if (MSGS > 2)
+		std::cout << "Bureaucrat::GradeTooHighException d.tor called" << std::endl;}
+
+const char *Bureaucrat::GradeTooHighException::what() const { return ("Bureaucrat grade too high"); }
+
+Bureaucrat::GradeTooLowException::GradeTooLowException(){
+	if (MSGS > 2)
+		std::cout << "Bureaucrat::GradeTooLowException default c.tor called" << std::endl;}
+
+Bureaucrat::GradeTooLowException::GradeTooLowException(const Bureaucrat::GradeTooLowException &ref){
+	if (MSGS > 2)
+		std::cout << "Bureaucrat::GradeTooLowException copy c.tor called" << std::endl;}
+
+Bureaucrat::GradeTooLowException &Bureaucrat::GradeTooLowException::operator=(const Bureaucrat::GradeTooLowException &rhs){
+	if (MSGS > 2)
+		std::cout << "Bureaucrat::GradeTooLowException copy assignment o.tor called" << std::endl;}
+
+Bureaucrat::GradeTooLowException::~GradeTooLowException(){
+	if (MSGS > 2)
+		std::cout << "Bureaucrat::GradeTooLowException d.tor called" << std::endl;}
+
+const char *Bureaucrat::GradeTooLowException::what() const { return ("Bureaucrat grade too low"); }
+
+//MEMBERS
 Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name)
 {
 	if (MSGS > 0)
 		std::cout << "Bureaucrat default c.tor called" << std::endl;
 	if (grade < 1 || grade > 150)
-		throw 
+		throw (this->GradeTooHighException::GradeTooHighException())
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat &ref) : _name(ref.getName())
 {
 	if (MSGS > 0)
-		std::cout << "Bureaucrat default c.tor called" << std::endl;
+		std::cout << "Bureaucrat copy c.tor called" << std::endl;
+	this->_grade = ref.getGrade();
 }
 
 Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs)
-{}
+{
+	if (MSGS > 0)
+		std::cout << "Bureaucrat copy assignment o.tor called" << std::endl;
+	this->_grade = rhs.getGrade();
+	return (*this);
+}
 
 Bureaucrat::~Bureaucrat()
-{}
+{
+	if (MSGS > 0)
+		std::cout << "Bureaucrat d.tor called" << std::endl;
+}
 
-const std::string const &Bureaucrat::getName() const
-{}
+std::ostream &Bureaucrat::operator<<(std::ostream &lhs) const
+{
+	lhs << this->_name << ", bureaucrat grade " << this->_grade;
+	return (lhs);
+}
 
-int const &Bureaucrat::getGrade() const
-{}
+const std::string const &Bureaucrat::getName() const { return (this->_name); }
+
+int const &Bureaucrat::getGrade() const { return (this->_grade); }
 
 void Bureaucrat::promote()
-{}
+{
+	if (this->_grade = 150)
+		throw ();
+	
+}
 
 void Bureaucrat::demote()
 {}
--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 19:31:18 by jchene            #+#    #+#             */
-/*   Updated: 2022/11/19 19:37:24 by jchene           ###   ########.fr       */
+/*   Updated: 2022/11/20 19:38:02 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,33 @@
 #define LIBPROTEC
 #include <iostream>
 #include <string>
+#include <exception>
 #endif
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 
 class Bureaucrat
 {
+private:
+	class GradeTooHighException : public std::exception
+	{
+		GradeTooHighException() throw();
+		GradeTooHighException(const GradeTooHighException &ref) throw();
+		GradeTooHighException &operator=(const GradeTooHighException &rhs) throw();
+		~GradeTooHighException() throw();
+		
+		const char *what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+		GradeTooLowException() throw();
+		GradeTooLowException(const GradeTooLowException &ref) throw();
+		GradeTooLowException &operator=(const GradeTooLowException &rhs) throw();
+		~GradeTooLowException() throw();
+		
+		const char *what() const throw();
+	};
+
 private:
 	const std::string _name;
 	int _grade;
@@ -33,10 +54,12 @@ public:
 	Bureaucrat &operator=(const Bureaucrat &rhs);
 	~Bureaucrat();
 
+	std::ostream &operator<<(std::ostream &lhs) const;
 	const std::string const &getName() const;
 	int const &getGrade() const;
 	void promote();
 	void demote();
+	
 };
 
 #endif
