30456a1ad606683558e2e9502d0a83c8175b1688
Julien CHÊNE <jchene@student.42.fr>
2022-12-21 19:40:46 +0100

    finished CPP07

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 19:49:42 by jchene            #+#    #+#             */
-/*   Updated: 2022/12/21 19:17:09 by jchene           ###   ########.fr       */
+/*   Updated: 2022/12/21 19:40:02 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBPROTEC
 # include <iostream>
 # include <string>
+# include <exception>
 #endif
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
@@ -22,66 +23,56 @@ template <typename T>
 class Array
 {
 private:
-	T				*_start;
-	unsigned int	_maxSize;
+	T	*_start;
+	int	_maxSize;
 
 public:
 
-	Array() : _start(NULL), _maxSize(0); {
+	Array<T>(void) : _start(NULL), _maxSize(0) {
 		if (MSGS > 0)
-			std::cout << "default c.tor called" << std::endl
+			std::cout << "default c.tor called" << std::endl;
 	};
 
-	Array(unsigned int n) : _start(NULL), _maxSize(0) {
+	Array<T>(unsigned int n) : _start(NULL), _maxSize(n) {
 		if (MSGS > 0)
-			std::cout << "uint c.tor called" << std::endl
+			std::cout << "uint c.tor called" << std::endl;
+		if (n < 0)
+			throw std::out_of_range("Index out of bonds");
 		this->_start = new T[n];
-		if (!this->_start)
-			return;
-		this->_maxSize = n;
 	};
 
-	Array(const Array &ref) _start(NULL), _maxSize(0) {
+	Array<T>(const Array &ref) : _start(NULL), _maxSize(0) {
 		if (MSGS > 0)
-			std::cout << "copy c.tor called" << std::endl
-		unsigned int size = ref.size();
-		this->_start = new T[size];
-		if (!this->_start)
-			return;
-		this->_maxSize = size;
-		for (unsigned int i = 0; i < this->_maxSize; i++)
-		{
-			(*this)[i] = ref[i];
-		}
+			std::cout << "copy c.tor called" << std::endl;
+		*this = ref;
 	};
 
-	Array &operator=(const Array &ref) {
+	Array<T> &operator=(const Array &ref) {
 		if (MSGS > 0)
-			std::cout << "o.tor = called" << std::endl
-		if (this->_start)
-			delete this->_start;
-		unsigned int size = ref.size();
-		this->_start = new T[size];
-		if (!this->_start)
-			return;
-		this->_maxSize = size;
-		for (unsigned int i = 0; i < this->_maxSize; i++)
-		{
-			(*this)[i] = ref[i];
-		}
+			std::cout << "assignment o.tor called" << std::endl;
+		if (this == &ref)
+			return (*this);
+		if (this->_maxSize > 0)
+			delete [] this->_start;
+		this->_maxSize = ref.size();
+		this->_start = new T[this->_maxSize];
+		for (int i = 0; i < this->_maxSize; i++)
+			this->_start[i] = ref[i];
+		return (*this);
 	};
 
-	const Array &operator[](int index)
+	T &operator[](const int index) const
 	{
-		if (index >= 0)
-			return (this->_start + index);
-		throw();
+		if (index >= 0 && index < this->_maxSize)
+			return (this->_start[index]);
+		throw std::out_of_range("Index out of bonds");
 	};
 
-	~Array() {
+	~Array<T>() {
 		if (MSGS > 0)
 			std::cout << "d.tor called" << std::endl;
-		free(this->_start);
+		if (this->_maxSize > 0)
+			delete [] this->_start;
 	};
 
 	unsigned int size() const {
