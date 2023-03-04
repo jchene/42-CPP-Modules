6632815681a45fe9bf417e3f59aa45706df15c28
Julien CHÊNE <jchene@student.42.fr>
2023-03-04 16:52:08 +0100

    doing cpp09/ex00

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -2,7 +2,10 @@
 #define BITCOINEXCHANGE_HPP
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <map>
+
+int exitPrint(const char *msg, int ret);
 
 #endif
--- CPP09/ex00/main.c
+++ CPP09/ex00/main.c
@@ -1,6 +1,10 @@
 #include "BitcoinExchange.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
+	if (argc != 2)
+		return (exitPrint("Bad number of arguments", -1));
+	std::ifstream database("db.csv");
+	database.open();
 	return (0);
 }
